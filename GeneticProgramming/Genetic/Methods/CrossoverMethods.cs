0dac16b4dc60d93a1b610b3b749b46828c48dd82
Dantre <dantreKrt@gmail.com>
2016-05-03 08:51:20 +0500

    Refactor: move crossoverspecies to tankStrategy

--- GeneticProgramming/Genetic/Methods/CrossoverMethods.cs
+++ GeneticProgramming/Genetic/Methods/CrossoverMethods.cs
@@ -21,9 +21,9 @@ namespace GeneticProgramming.Genetic.GeneticEngine
 
             for (int i = 0; i < panmixiaCount; i++)
             {
-                var specimen1 = basePopulation[random.Next(basePopulation.Count)];
-                var specimen2 = basePopulation[random.Next(basePopulation.Count)];
-                yield return CrossoverSpecies(specimen1, specimen2);
+                var strategy1 = basePopulation[random.Next(basePopulation.Count)];
+                var strategy2 = basePopulation[random.Next(basePopulation.Count)];
+                yield return strategy1.CrossoverSpecies(strategy2, configuration.MaximumProgramSize);
             }
         }
 
@@ -34,9 +34,9 @@ namespace GeneticProgramming.Genetic.GeneticEngine
 
             for (int i = 0; i < inbreedCount; i++)
             {
-                var specimen1 = basePopulation[random.Next(basePopulation.Count)];
-                var specimen2 = FindMostLikely(basePopulation, specimen1);
-                yield return CrossoverSpecies(specimen1, specimen2);
+                var strategy1 = basePopulation[random.Next(basePopulation.Count)];
+                var strategy2 = strategy1.FindMostLikely(basePopulation);
+                yield return strategy1.CrossoverSpecies(strategy2, configuration.MaximumProgramSize);
             }
         }
 
@@ -47,52 +47,10 @@ namespace GeneticProgramming.Genetic.GeneticEngine
 
             for (int i = 0; i < outbreedCount; i++)
             {
-                var specimen1 = basePopulation[random.Next(basePopulation.Count)];
-                var specimen2 = FindMostUnlikely(basePopulation, specimen1);
-                yield return CrossoverSpecies(specimen1, specimen2);
+                var strategy1 = basePopulation[random.Next(basePopulation.Count)];
+                var strategy2 = strategy1.FindMostLikely(basePopulation);
+                yield return strategy1.CrossoverSpecies(strategy2, configuration.MaximumProgramSize);
             }
         }
-
-        public TankStrategy CrossoverSpecies(TankStrategy specimen1, TankStrategy specimen2)
-        {
-            var random = new Random(Guid.NewGuid().GetHashCode());
-            int firstIndex = random.Next(specimen1.commands.Count);
-            int secondIndex = random.Next(specimen2.commands.Count);
-
-            if (firstIndex + secondIndex > configuration.MaximumProgramSize)
-            {
-                int diff = (firstIndex + secondIndex) - configuration.MaximumProgramSize;
-                if (diff%2 == 1) diff++;
-                firstIndex -= diff << 1;
-                secondIndex -= diff << 1;
-            }
-
-            var firstPart = specimen1.commands.Take(firstIndex).ToList();
-            var secondPart = specimen2.commands.Skip(specimen2.commands.Count - secondIndex).Take(secondIndex);
-            firstPart.AddRange(secondPart);
-            
-            return new TankStrategy(firstPart);
-        }
-        
-        public TankStrategy FindMostLikely(List<TankStrategy> basePopulation, TankStrategy specimen)
-        {
-            return basePopulation.Where(algo => algo != specimen)
-                    .Max(algo => Tuple.Create(HammingDistance(algo, specimen), algo)).Item2;
-        }
-        public TankStrategy FindMostUnlikely(List<TankStrategy> basePopulation, TankStrategy specimen)
-        {
-            return basePopulation.Where(algo => algo != specimen)
-                    .Min(algo => Tuple.Create(HammingDistance(algo, specimen), algo)).Item2;
-        }
-
-        public int HammingDistance(TankStrategy algo, TankStrategy specimen)
-        {
-            int count = 0;
-            for (int i = 0; i < Math.Min(algo.commands.Count, specimen.commands.Count); i++)
-                if (algo.commands[i] == specimen.commands[i])
-                    count++;
-            return count;
-        }
-      
     }
 }
--- GeneticProgramming/Tank/TankStrategy.cs
+++ GeneticProgramming/Tank/TankStrategy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GeneticProgramming.Tank
@@ -27,6 +28,48 @@ namespace GeneticProgramming.Tank
             this.commands = commands;
         }
 
+        public TankStrategy CrossoverSpecies(TankStrategy anotherStrategy, int maxSize)
+        {
+            var random = new Random(Guid.NewGuid().GetHashCode());
+            int firstIndex = random.Next(commands.Count);
+            int secondIndex = random.Next(anotherStrategy.commands.Count);
+
+            if (firstIndex + secondIndex > maxSize)
+            {
+                int diff = (firstIndex + secondIndex) - maxSize;
+                if (diff % 2 == 1) diff++;
+                firstIndex -= diff << 1;
+                secondIndex -= diff << 1;
+            }
+
+            var firstPart = commands.Take(firstIndex).ToList();
+            var secondPart = anotherStrategy.commands.Skip(anotherStrategy.commands.Count - secondIndex).Take(secondIndex);
+            firstPart.AddRange(secondPart);
+
+            return new TankStrategy(firstPart);
+        }
+
+        public TankStrategy FindMostLikely(List<TankStrategy> basePopulation)
+        {
+            return basePopulation.Where(algo => algo != this)
+                    .Max(algo => Tuple.Create(HammingDistance(algo), algo)).Item2;
+        }
+        public TankStrategy FindMostUnlikely(List<TankStrategy> basePopulation)
+        {
+            return basePopulation.Where(algo => algo != this)
+                    .Min(algo => Tuple.Create(HammingDistance(algo), algo)).Item2;
+        }
+
+        public int HammingDistance(TankStrategy anotherStrategy)
+        {
+            int count = 0;
+            for (int i = 0; i < Math.Min(commands.Count, anotherStrategy.commands.Count); i++)
+                if (commands[i] == anotherStrategy.commands[i])
+                    count++;
+            return count;
+        }
+
+        #region EqualityComparer
         private sealed class CommandsEqualityComparer : IEqualityComparer<TankStrategy>
         {
             public bool Equals(TankStrategy x, TankStrategy y)
@@ -50,6 +93,6 @@ namespace GeneticProgramming.Tank
         {
             get { return CommandsComparerInstance; }
         }
+        #endregion
     }
-
 }
