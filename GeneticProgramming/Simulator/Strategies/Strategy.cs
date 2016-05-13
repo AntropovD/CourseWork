d601d1d5f536370c52a4bf78e88d2bda5a016e8c
Dantre <dantreKrt@gmail.com>
2016-05-13 09:08:40 +0500

    start working

---
+++ GeneticProgramming/Simulator/Strategies/Strategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneticProgramming.Simulator.Strategies
+{
+    public class Strategy : IComparable
+    {
+        public List<string> commands;
+        public int index;
+
+        public Strategy(IEnumerable<string> commands)
+        {
+            this.commands = commands.ToList();
+            index = 0;
+        }
+
+        public Strategy(List<string> commands)
+        {
+            this.commands = commands;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ GeneticProgramming/Visualiser/ConsoleVisualiser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GeneticProgramming.Simulator.Maps;
+
+namespace GeneticProgramming.Visualiser
+{
+    class ConsoleVisualiser : BaseVisualiser
+    {
+        public override void Visualise(Map map)
+        {
+            Console.Clear();
+            string[] field = GetField(map);
+            foreach (var s in field)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
